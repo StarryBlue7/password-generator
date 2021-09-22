5d420500b847e8d0be6eba7d3ccfcfc689edbaac
Vince <starryblue7@gmail.com>
2021-09-21 20:32:58 -0700

    add queries for length and character types

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var length = function queryLength() {
+  length = prompt("How many characters long will your password be?", "Must be a number 8-128");
+  if (length == null) {
+    exit();
+  } else if (typeof length !== "number" || length <= 8 || length >= 128) {
+    alert("Error: Not a number 8-128.");
+    queryLength();
+  }
+}
+
+var upperCase = function queryUpperCase() {
+  prompt("Will it include upper case letters? Click OK for yes or Cancel for no.");
+}
+
+var lowerCase = function queryLowerCase() {
+  prompt("Will it include lower case letters? Click OK for yes or Cancel for no.");
+}
+
+var numeric = function queryNumeric() {
+  prompt("Will it include numbers? Click OK for yes or Cancel for no.");
+}
+
+var special = function querySpecial() {
+  prompt("Will it include special characters? Click OK for yes or Cancel for no.");
+}
