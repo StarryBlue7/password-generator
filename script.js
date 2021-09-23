6e0cf55208087fdeed441b2a3f112007ade6347e
Vince <starryblue7@gmail.com>
2021-09-23 09:37:59 -0700

    add comments describing functionality

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Prompt for length of password, gives an error message and re-prompts if input is invalid
 function queryLength() {
   const lowerLimit = 8;
   const upperLimit = 128;
@@ -30,6 +31,7 @@ function queryLength() {
   return inputLength;
 }
 
+// Confirm types of characters to be included
 function queryUpperCase() {
   return confirm("Will it include upper case letters? Click OK for yes or Cancel for no.");
 }
@@ -46,6 +48,7 @@ function querySpecial() {
   return confirm("Will it include special characters? Click OK for yes or Cancel for no.");
 }
 
+// Generate upper or lower case letter
 function generateLetter(isUpper) {
   const randLetter = Math.floor(Math.random() * 26);
   if (isUpper) {
@@ -57,16 +60,19 @@ function generateLetter(isUpper) {
   }
 }
 
+// Generate symbol
 function generateSymbol() {
   const symbols = ['!','@','#','$','%','^','&','*','(',')','+','\\','/','\'','?',':',',','{','}','[',']','~','`','-','_','.'];
   const randIndex = Math.floor(Math.random() * symbols.length);
   return symbols[randIndex];
 }
 
+// Generate number
 function generateNumeric() {
   return Math.floor(Math.random() * 10);
 }
 
+// Find random empty slots in password array
 function findRandEmptySlot(characters) {
   let slot;
   do {
@@ -75,7 +81,10 @@ function findRandEmptySlot(characters) {
   return slot;
 }
 
+// Main password generation function
 function generatePassword() {
+
+  // Initial queries, quit on "Cancel" for length input or "Cancel" for all character types
   const length = queryLength();
   if (length === null) {
     return null;
@@ -85,16 +94,20 @@ function generatePassword() {
   const lowerCase = queryLowerCase();
   const numeric = queryNumeric();
   const special = querySpecial();
+
   if (upperCase === false && lowerCase === false && numeric === false && special === false) {
     alert("No character types chosen.");
     return null;
   }
 
+  // Create empty array of desired length for password characters
   let characters = [];
   for (i = 0; i < length; i++) {
     characters.push(undefined);
   }
 
+  // Generate at least one of each required character type at random empty index
+  // and create array of required character generators
   let generators = [];
   if (upperCase) {
     const slot = findRandEmptySlot(characters);
@@ -124,6 +137,7 @@ function generatePassword() {
     generators.push(() => { return generateSymbol();});
   }
 
+  // Randomly choose other required character types and fill remaining empty indices
   for (let i = 0; i < length; i++) {
     if (characters[i] === undefined) {
       const charType = Math.floor(Math.random() * generators.length);
@@ -131,5 +145,6 @@ function generatePassword() {
     }
   }
 
+  // Return string from filled password characters array
   return characters.join('');
 }
